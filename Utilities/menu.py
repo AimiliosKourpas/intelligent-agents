e893f8efd31dd74c0f404e888f0fa896cf0d9b23
Aimilios Kourpas <aimiliosk2002@gmail.com>
2024-06-03 15:17:39 +0300

    some changes to the code

--- Utilities/menu.py
+++ Utilities/menu.py
@@ -1,13 +1,11 @@
 from Utilities.SearchAlgorithms import bfs, a_star
 from Utilities.Utilities import gcd, _exit, state_generator
 from Classes.BlocksWorld.BlocksWorldH2 import BlocksWorldH2
-
-
 from Utilities.Constants import MSG_SELECT_GAME, ALLOWED_ANSWERS, DEFAULT_ANSWER
 from Utilities.Constants import MSG_INPUT_1, MSG_INPUT_2, MSG_INPUT_TARGET, MSG_NO_POSSIBLE_SOLUTION_WATER
 
 
-def jugsProblem():
+def jugs_problem():
     jug_1_capacity = int(input(MSG_INPUT_1))
     jug_2_capacity = int(input(MSG_INPUT_2))
     final_destination = int(input(MSG_INPUT_TARGET))
@@ -16,54 +14,54 @@ def jugsProblem():
         print(MSG_NO_POSSIBLE_SOLUTION_WATER)
         return
 
-    print(bfs(final_destination, jug_1_capacity, jug_2_capacity))
+    solution = bfs(final_destination, jug_1_capacity, jug_2_capacity)
+    print(solution)
 
 
-def blockWorld():
+def block_world():
     print("Block world problem")
-    blocks = int(input("Number of blocks : "))
+    blocks = int(input("Number of blocks: "))
 
-    initial_state = state_generator(blocks)  # Generating random states for initial and goal state
+    initial_state = state_generator(blocks)  # Generate random states for initial and goal state
     goal_state = state_generator(blocks)
-    print("Initial: {}".format(initial_state))
-    print("Goal: {}".format(goal_state))
+    print(f"Initial: {initial_state}")
+    print(f"Goal: {goal_state}")
 
-    if initial_state is goal_state:
-        print("Initial State is the same with the Goal State")
+    if initial_state == goal_state:
+        print("Initial State is the same as the Goal State")
         return
 
-    problem2 = BlocksWorldH2(initial_state, goal_state)
+    problem = BlocksWorldH2(initial_state, goal_state)
 
-    a_star_results = a_star(problem2)  # A* search using second heuristic
-    a_star_solution = a_star_results.solution()
-    a_star_path = a_star_results.path()
+    result = a_star(problem)  # A* search using second heuristic
+    solution = result.solution()
+    path = result.path()
 
-    print("{:=^41}".format("Solution"))
-    print("{:^16}{:^5}{:<50}".format("ACTION", '|', "STATE"))
-    print("{:<16}{:^5}{:<50}".format('Initial State', '|', str(initial_state)))
+    print("{:=^41}".format(" Solution "))
+    print("{:^16} | {:<50}".format("ACTION", "STATE"))
+    print("{:<16} | {:<50}".format('Initial State', str(initial_state)))
 
-    for iterator in range(1, len(a_star_path)):
-        state = a_star_path[iterator].state
-        action = a_star_solution[iterator - 1]
+    for i in range(1, len(path)):
+        state = path[i].state
+        action = solution[i - 1]
 
         if action[1] != ' ':
-            msg = "Move from {} to {}".format(action[0], action[1])
+            msg = f"Move from {action[0]} to {action[1]}"
         else:
-            msg = "Move {} down".format(action[0])
+            msg = f"Move {action[0]} down"
 
-        print("{:<16}{:^5}{:<50}".format(msg, '|', str(state)))
+        print("{:<16} | {:<50}".format(msg, str(state)))
 
 
-def showMenu():
-    flag, problem = True, DEFAULT_ANSWER
-    while flag:
-        problem = str(input(MSG_SELECT_GAME))
+def show_menu():
+    while True:
+        problem = input(MSG_SELECT_GAME).strip().upper()
         if problem in ALLOWED_ANSWERS:
-            flag = False
+            break
 
     if problem == "1":
-        jugsProblem()
+        jugs_problem()
     elif problem == "2":
-        blockWorld()
-    elif problem.upper() == "Q":
+        block_world()
+    elif problem == "Q":
         _exit()
